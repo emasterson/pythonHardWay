3232657cd1da0c0e899ab720a0502354f947c867
Chris Masterson <chrismasterson@Chriss-MacBook-Pro.local>
2016-07-07 17:14:03 -0500

    adding ex13.py

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "script called: ", script
+print "your first variable is: ", first
+print "your second variable is: ", second
+print "your 3rd variable is : ", third
+
